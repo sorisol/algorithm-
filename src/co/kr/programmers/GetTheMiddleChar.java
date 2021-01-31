747f0206ead91a7bf45f3a029d576e6b5bb83d1f
sorisol <6.6yesol@gmail.com>
2021-01-31 22:34:08 +0900

    프로그래머스 - 가운데 글자 가져오기

---
+++ src/co/kr/programmers/GetTheMiddleChar.java
@@ -0,0 +1,28 @@
+package co.kr.programmers;
+
+public class GetTheMiddleChar {
+
+	public static void main(String[] args) {
+		GetTheMiddleChar g = new GetTheMiddleChar();
+		
+		String s = "abcde";
+		System.out.println(g.solution(s));
+		
+		s = "qwer";
+		System.out.println(g.solution(s));
+		
+	}
+	public String solution(String s) {
+        String answer = "";
+        
+        if(s.length() % 2 == 0) {
+            int start = s.length()/2 - 1;
+            System.out.println(start);
+            answer = s.substring(start,	start+2);
+        } else {
+            answer = String.valueOf(s.charAt(s.length()/2));
+        }
+        
+        return answer;
+    }
+}
